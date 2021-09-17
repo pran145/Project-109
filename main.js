3974415f37eeb6cded496d3cc2b40377f6ec9621
pran145 <77706918+pran145@users.noreply.github.com>
2021-09-16 20:01:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/QKRuEb5Ov/model.json', modelReady);
+}
+
+function modelReady (){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      random_number_r = Math.floor(Math.random() * 255) + 1;
+      random_number_g = Math.floor(Math.random() * 255) + 1;
+      random_number_b = Math.floor(Math.random() * 255) + 1;
+  
+      document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+      document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+      document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+      document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+  
+      img = document.getElementById('alien 1') 
+      img1 = document.getElementById('alien 2')
+      img2 = document.getElementById('alien 3')
+      img3 = document.getElementById('alien 4')
+  
+      if (results[0].label == "Bark") {
+        img.src = 'bark.gif';
+        img1.src = '8666253_0.jpg';
+        img2.src = 'noise-illustration-1024x710.jpg';
+        img3.src = 'cat-cute.png';
+      } 
+      if (results[0].label == "Moo") {
+        img.src = 'puppy-development.jpg';
+        img1.src = 'cow-pick.gif';
+        img2.src = 'noise-illustration-1024x710.jpg';
+        img3.src = 'cat-cute.png';
+      } 
+      if (results[0].label == "Background Noise") {
+        img.src = 'puppy-development.jpg';
+        img1.src = '8666253_0.jpg';
+        img2.src = 'listen.gif';
+        img3.src = 'cat-cute.png';
+      } 
+      if (results[0].label == "meow") {
+        img.src = 'puppy-development.jpg';
+        img1.src = '8666253_0.jpg';
+        img2.src = 'noise-illustration-1024x710.jpg';
+        img3.src = 'meow.gif';
+      } 
+    }
+  }
+  
